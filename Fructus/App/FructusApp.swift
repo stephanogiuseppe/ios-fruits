0db3d439a2fc57ad9e46d6400455e2f97eb5360a
Stéphano Giuseppe Rajão de Paiva <stephanogiuseppe@MacBook-Air-de-Stephano.local>
2021-03-06 13:44:36 -0300

    add assets, simple scroll and style

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Stéphano Giuseppe Rajão de Paiva on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ForEach(0..<5) { item in FruitCardView() }
+            FruitCardView()
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
     }
 }
 
