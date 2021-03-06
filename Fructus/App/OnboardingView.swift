b4481682666fdaf555d1b575b1049d3d673249f3
Stéphano Giuseppe Rajão de Paiva <stephanogiuseppe@MacBook-Air-de-Stephano.local>
2021-03-06 13:43:49 -0300

    init project

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Stéphano Giuseppe Rajão de Paiva on 06/03/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Stéphano Giuseppe Rajão de Paiva on 06/03/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}
